e1b7a162654d881269c9e24c1e1f23e329f4ea8b
dilshan <dilshan.b@softcodeit.com>
2021-05-09 13:42:42 +0530

    second4

--- src/main/java/lk/codelabs/samples/microservicesamplebasic/controller/ApplicationController.java
+++ src/main/java/lk/codelabs/samples/microservicesamplebasic/controller/ApplicationController.java
@@ -12,7 +12,7 @@ public class ApplicationController {
 
         Response response = new Response();
         response.setCode("SAMPLE_CODE");
-        response.setMessage("welcome to microservices dilz springboot 2.0");
+        response.setMessage("welcome to microservices dilz Kavi springboot 2.0");
         return response;
 
     }
