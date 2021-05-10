f0399d1f32aa670c9949d2e3bc1f6b055e881199
dilshan <dilshan.b@softcodeit.com>
2021-05-10 20:59:49 +0530

    test102

--- src/main/java/lk/codelabs/samples/microservicesamplebasic/controller/ApplicationController.java
+++ src/main/java/lk/codelabs/samples/microservicesamplebasic/controller/ApplicationController.java
@@ -12,7 +12,7 @@ public class ApplicationController {
 
         Response response = new Response();
         response.setCode("SAMPLE_CODE");
-        response.setMessage("Dilshan welcome to microservices with ANSIBLE and  springboot 2.0");
+        response.setMessage("Dilshan Boteju welcome to microservices with ANSIBLE and  springboot 2.0");
         return response;
 
     }
